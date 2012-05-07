8aaa962ff1e48e9e8be564259a19bc2aaddc45fd
Yen-Ju Chen <yjchenx@gmail.com>
2012-05-07 15:12:15 +0800

    remove unused code

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,17 +1,4 @@
-#require File.expand_path(File.join(File.dirname(__FILE__), %w[.. lib ri_cal]))
 require 'ri_cal'
-require 'cgi'
-require 'tzinfo'
-
-module Kernel
-  if ENV.keys.find {|env_var| env_var.match(/^TM_/)}
-    def rputs(*args)
-      puts( *["<pre>", args.collect {|a| CGI.escapeHTML(a.to_s)}, "</pre>"])
-    end
-  else
-    alias_method :rputs, :puts
-  end
-end
 
 def date_time_with_zone(date_time, tzid = "US/Eastern")
   date_time.dup.set_tzid(tzid)
