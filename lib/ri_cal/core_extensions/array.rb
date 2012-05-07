f0ca4939ba5ebff5624a26c19f6b52f09b9d7ae1
Yen-Ju Chen <yjchenx@gmail.com>
2012-05-07 14:55:25 +0800

    simplify core_extensions

--- lib/ri_cal/core_extensions/array.rb
+++ lib/ri_cal/core_extensions/array.rb
@@ -1,4 +1,16 @@
-require "ri_cal/core_extensions/array/conversions.rb"
+module RiCal
+  module CoreExtensions #:nodoc:
+    module Array #:nodoc:
+      module Conversions
+        # return the concatenation of the elements representation in rfc 2445 format
+        def to_rfc2445_string # :doc:
+          join(",")
+        end
+      end
+    end
+  end
+end
+
 class Array #:nodoc:
   include RiCal::CoreExtensions::Array::Conversions
 end
--- lib/ri_cal/core_extensions/object.rb
+++ lib/ri_cal/core_extensions/object.rb
@@ -1,4 +1,20 @@
-require "ri_cal/core_extensions/object/conversions.rb"
+module RiCal
+  module CoreExtensions #:nodoc:
+    module Object #:nodoc:
+      module Conversions #:nodoc:
+        # Used to format rfc2445 output for RiCal
+        def to_rfc2445_string
+          to_s
+        end
+        
+        # Used by RiCal specs returns the receiver
+        def to_ri_cal_ruby_value
+          self
+        end
+      end
+    end
+  end
+end
 
 class Object #:nodoc:
   include RiCal::CoreExtensions::Object::Conversions
